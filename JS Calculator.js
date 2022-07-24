e15573d05fdaa279e2eb3d4a584156e67be4d3ba
Jacob Paquette <jacobpaquette33@gmail.com>
2022-07-23 21:26:57 -0400

    first ever commit

---
+++ JS Calculator.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", function() {
+
+firstNumber = prompt("Please enter the first value");
+secondNumber = prompt("Please enter the second value");
+var sum = Number(firstNumber) + Number(secondNumber);
+alert("The sum of these values = " + sum);
+
+alert("Now we are going to subtract two values!")
+thirdNumber = prompt("Please enter the first value");
+fourthNumber = prompt("Please enter the second value");
+var difference = Number(thirdNumber) - Number(fourthNumber);
+alert("The difference of these values = " + difference);
+
+alert("Now we are going to subtract two values!")
+fifthNumber = prompt("Please enter the first value");
+sixthNumber = prompt("Please enter the second value");
+var product = Number(fifthNumber) * Number(sixthNumber);
+alert("The product of these values = " + product);
+
+alert("Now we are going to subtract two values!")
+seventhNumber = prompt("Please enter the first value");
+eighthNumber = prompt("Please enter the second value");
+var quotient = Number(seventhNumber) / Number(eighthNumber);
+alert("The quotient of these values = " + quotient);
+});
